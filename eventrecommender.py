31284454c192289fe0f10718fd7013b8393991c2
Edgar Velazquez <edantonio505@gmail.com>
2022-03-06 10:58:22 -0500

    refactoring of interactive page

--- eventrecommender.py
+++ eventrecommender.py
@@ -4,23 +4,21 @@ from recommender import Recommender
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-t', '--today', help='Show today\'s events [True|False]')
+    parser.add_argument('-t',  '--today' ,action='store_true', help='Show today\'s events ')
     args = parser.parse_args()
-    
-    if args.today == None:
-        parser.parse_args(['-h'])
-        quit()
-    if args.today != "True" and args.today != "False":
-        print("The today arguments should be either 'True' or 'False'")
-        parser.parse_args(['-h'])
-        quit()
-    today = True if args.today == "True" else False
-    os.system('cls' if os.name == 'nt' else 'clear')
-    print("just choose this!")
-    print("==================")
-    recommender = Recommender(today)    
-    message = recommender.get_recommendation()
-    print(message)
+
+    if args.today:
+        today = args.today
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print("just choose this!")
+        print("==================")
+        recommender = Recommender(today)    
+        message = recommender.get_recommendation()
+        print(message)
+    else:
+        # print help
+        parser.print_help()
+        
 
 if __name__ == "__main__":
     main()
