3c69d2119101432411e1390c7633d2218da4af8a
Edgar Velazquez <edantonio505@gmail.com>
2022-04-03 02:36:38 -0400

    fixing the command line

--- eventrecommender.py
+++ eventrecommender.py
@@ -3,22 +3,25 @@ import argparse
 from recommender import Recommender
 
 def main():
+    parser = argparse.ArgumentParser(description='Nyc Event Recommender')
+
     parser = argparse.ArgumentParser()
-    parser.add_argument('-t',  '--today' ,action='store_true', help='Show today\'s events ')
+    parser.add_argument('-t',  '--today' ,action='store_true', help='Show today\'s events')
+    parser.add_argument('-a',  '--all' ,action='store_true', help='Show events all week')
+    parser.add_argument('-j',  '--json' ,action='store_true', help='Show events in json format')
     args = parser.parse_args()
 
-    if args.today:
-        today = args.today
+    if args.today or args.all:
         os.system('cls' if os.name == 'nt' else 'clear')
-        print("just choose this!")
-        print("==================")
-        recommender = Recommender(today)    
-        message = recommender.get_recommendation()
-        print(message)
+        today = args.today
+        json = args.json
+        recommender = Recommender(today, json)
+        events = recommender.get_recommendation()
+        print(events)
+
     else:
-        # print help
         parser.print_help()
-        
+
 
 if __name__ == "__main__":
     main()
